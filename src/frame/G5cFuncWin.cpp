ffc1e32ebb1f0c9cb8ce08b5e01254db95b12064
hongfu <1342735006@qq.com>
2020-12-02 23:43:42 +0800

    Base Class

---
+++ src/frame/G5cFuncWin.cpp
@@ -0,0 +1,9 @@
+#include "src/frame/G5cFuncWin.h"
+
+using namespace g5c::frame;
+
+G5cFuncWin::G5cFuncWin(QWidget *parent)
+    : QWidget(parent)
+{
+
+}
--- src/frame/G5cMainWin.cpp
+++ src/frame/G5cMainWin.cpp
@@ -1,6 +1,5 @@
 #include "src/frame/G5cMainWin.h"
 
-
 using namespace g5c::frame;
 
 G5cMainWin::G5cMainWin(QWidget *parent)
